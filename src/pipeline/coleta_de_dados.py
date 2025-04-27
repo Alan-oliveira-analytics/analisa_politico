39b7b57a3ec0225eaebc04062f46b3a08b951d40
Alan <alandeolivieradev@gmail.com>
2025-04-27 10:26:20 -0300

    Alteracoes na coleta de dados - pipeline.py

--- src/pipeline/coleta_de_dados.py
+++ src/pipeline/coleta_de_dados.py
@@ -18,9 +18,25 @@ import os
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 # Pasta de dados (dentro da raiz)
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-os.makedirs(DATA_DIR, exist_ok=True)
 
+if not os.path.exists(DATA_DIR):
+    print(f"O diretório {DATA_DIR} não existe, criando...")
+    os.makedirs(DATA_DIR, exist_ok=True)
+else:
+    print(f"O diretório {DATA_DIR} já existe.")
+
+# Testando a criação de um arquivo dentro do diretório
+try:
+    test_file_path = os.path.join(DATA_DIR, "test.txt")
+    with open(test_file_path, 'w') as f:
+        f.write("Teste de gravação!")
+    print(f"Arquivo criado com sucesso: {test_file_path}")
+except OSError as e:
+    print(f"Erro ao tentar gravar no diretório: {e}")
+
+    
 """ ----------------IMPORTAÇÃO CLASSE---------------- """
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from src.api.deputados_api import deputadoAPI
@@ -36,7 +52,7 @@ dados = api.get_dados(endpoint='deputados')
 dados = dados['dados']
 
 df = pd.DataFrame(dados)
-# df.to_csv('./data/tb_deputados.csv', index=False)
+df.to_csv('./data/tb_deputados.csv', index=False)
 
 
 
@@ -64,7 +80,9 @@ deputados_informacoes.head()
 
 deputados_informacoes.to_csv('./data/info_deputados.csv')
 
-coluna_id = [204379, 220714, 221328]
+
+
+
 # Pegando as informações de despesa
 despesas_deputados = {}
 for x in coluna_id:
@@ -72,23 +90,30 @@ for x in coluna_id:
     dados = api.get_dados(endpoint=f'deputados/{x}/despesas')
     dados = dados['dados']
     despesas_deputados[x] = dados  # Use 'x' (deputy ID) as the key
-print(despesas_deputados)
-
-despesas_deputados = pd.DataFrame(despesas_deputados)
-# despesas_deputados = despesas_deputados.T
-# despesas_deputados.reset_index(drop=True, inplace=True)
-print(despesas_deputados)
-despesas_deputados.to_csv('./data/info_despesas.csv')
 
 
+# Inicializar uma lista para armazenar os DataFrames gerados
+df_list = []
+
+# Iterar sobre cada índice do dicionário
+for index, records in despesas_deputados.items():
+    # Expandir cada lista de dicionários para um DataFrame
+    df_temp = pd.json_normalize(records)
+    
+    # Adicionar uma coluna com o índice atual
+    df_temp['indice'] = index
+    
+    # Adicionar o DataFrame à lista
+    df_list.append(df_temp)
+
+# Concatenar todos os DataFrames na lista em um único DataFrame
+df_final = pd.concat(df_list, ignore_index=True)
+
+df_final.head()
+df_final.shape
 
 
-
-
-
-
-
-
+df_final.to_csv('./data/despesas_deputados.csv')
 
 
 
